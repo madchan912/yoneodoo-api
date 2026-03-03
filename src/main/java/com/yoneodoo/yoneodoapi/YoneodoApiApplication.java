2b4e42e3a54ee16cc0ccee7773dd4ad7b1ab6cc7
madchan912 <madchan912@gmail.com>
2026-03-03 09:57:16 +0900

    chore: Spring Boot 초기 프로젝트 세팅 및 Docker/DB 설정

---
+++ src/main/java/com/yoneodoo/yoneodoapi/YoneodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.yoneodoo.yoneodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoneodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoneodoApiApplication.class, args);
+    }
+
+}
