895887ae6ea0682c781c00eb0876840f9080d7f7
madchan912 <madchan912@gmail.com>
2026-03-12 15:30:38 +0900

    chore: 프로젝트 패키지 및 클래스명 오타 수정 (yoneodo -> yoneodoo)

--- src/main/java/com/yoneodoo/api/YoneodooApiApplication.java
+++ src/main/java/com/yoneodoo/api/YoneodooApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YoneodoApiApplication {
+public class YoneodooApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YoneodoApiApplication.class, args);
+        SpringApplication.run(YoneodooApiApplication.class, args);
     }
 
 }
